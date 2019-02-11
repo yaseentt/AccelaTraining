a6558d23f68f7c1ef0d46cd8b651e7f634510437
hjamous <hjamous@accela.com>
2019-02-11 15:04:06 +0200

    test new repo

--- src/com/accela/training/Main.java
+++ src/com/accela/training/Main.java
@@ -5,5 +5,12 @@ public class Main {
     public static void main(String[] args) {
 	    // write your code here
         System.out.println("Hello World 2");
+        System.out.println(haniMethod("By hani");
+    }
+    
+    public String haniMethod(String P1)
+    {
+    	String p2="testing Git"+P1;
+    	return p2;
     }
 }
