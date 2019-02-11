5317c7bcb7cbc91b6678de24eb40b9c859221737
mhashaikeh <mhashaikeh@accela.com>
2019-02-11 16:15:05 +0200

    testtttt

--- src/com/accela/training/TestClass.java
+++ src/com/accela/training/TestClass.java
@@ -1,7 +1,12 @@
 package com.accela.training;
 
 public class TestClass {
-    public static void TestMethod() {
+	
+	TestClass(){
+		
+	}
+	
+    public void TestMethod() {
         System.out.println("This is my own test method, By Hashaikeh");
 		
 		for(int i=1; i<=10; i++){
