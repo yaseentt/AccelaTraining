bb807a529747c1a603865eb2b9aca0212d9c7635
rbader2018 <rbader@accela.com>
2019-02-11 15:13:04 +0200

    test 3

--- src/com/accela/training/TestClass.java
+++ src/com/accela/training/TestClass.java
@@ -2,7 +2,7 @@ package com.accela.training;
 
 public class TestClass {
     public static void TestMethod() {
-        System.out.println("This is my own test method");
+        System.out.println("This is my own test method, updated by rami");
 		
 		for(int i=1; i<=10; i++){
 			System.out.println("Hashaikeh Test: #" + i);
