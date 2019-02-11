f7a0253fdcea777d59687928c7b5f7e2144d483b
mhashaikeh <mhashaikeh@accela.com>
2019-02-11 15:02:39 +0200

    Calling TestClass

--- src/com/accela/training/Main.java
+++ src/com/accela/training/Main.java
@@ -5,5 +5,8 @@ public class Main {
     public static void main(String[] args) {
 	    // write your code here
         System.out.println("Hello World 2");
+		
+		TestClass testClass = new TestClass();
+		testClass.TestClass();
     }
 }
