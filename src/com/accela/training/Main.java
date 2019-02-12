6cf9aa3dfcbd7b294948ffd62900614da9f7f76a
hjamous <hjamous@accela.com>
2019-02-12 12:09:25 +0200

    Uodated By Hani for Adding Method returnMyName

--- src/com/accela/training/Main.java
+++ src/com/accela/training/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	    // write your code here
         System.out.println("Hello World 2");
-		
+		System.out.println(returnMyName("Hani"));
 		Main m = new Main();
     }
 	
@@ -13,4 +13,10 @@ public class Main {
 		TestClass t = new TestClass();
 		t.TestClass();
 	}
+	
+	public String returnMyName(String P1)
+	{
+		return P1;
+	}
+	
 }
