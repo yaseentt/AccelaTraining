a01e4d4199ae37157a9bb76debb41be737105bbd
Yaseen Altiti <ytiti@accela.com>
2019-02-12 16:21:58 +0400

    Test

--- src/com/accela/training/Main.java
+++ src/com/accela/training/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	    // write your code here
-        System.out.println("Hello World 2");
+        System.out.println("Hello World 3");
 		System.out.println(returnMyName("Hani"));
 		Main m = new Main();
     }
